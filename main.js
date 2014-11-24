c0057598a64620bb531525fd8dda7063e9e66e81
darren-yu <darren.d.yu@gmail.com>
2014-11-24 09:34:43 -0800

    updated again for cleaner code

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var turns = 0;
+	var checkWinner = false;
 	var resetCounter = 0;
 	var squares = document.getElementsByClassName('sq');
 	var winArray = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[6,4,2]];
 	var resetButton = document.getElementById('reset');
 
-	//the below adjust color of cells upon clicking
-	var tableCells = document.querySelectorAll("#cell");
-
-	for (var i = 0; i < tableCells.length; i++) {
-		tableCells[i].addEventListener("click", function(){
-			this.style.backgroundColor="lightskyblue";
-		});
-
-	};
-
-
+	
 	var winner = function() {
 		for (var i = 0; i < winArray.length; i++) {
 			if ((squares[winArray[i][0]].innerText !== "--") && 
 			(squares[winArray[i][1]].innerText === squares[winArray[i][0]].innerText) &&
 			(squares[winArray[i][2]].innerText === squares[winArray[i][0]].innerText)) {
+				checkWinner = true;
 				alert(squares[winArray[i][1]].innerText + " wins!");
-				return squares[winArray[i][1]].innerText;
+				return;
 			}
 		}
 	};
@@ -31,18 +23,17 @@ document.addEventListener("DOMContentLoaded", function(){
 	resetButton.addEventListener("click",function(){
 	// document.getElementById('reset').addEventListener("click",function(){
 		// // alert("resetting...");
+		resetCounter += 1;
 		for(var i = 0; i < squares.length; i++) {
 			squares[i].innerText = "--";
-		}
-		resetCounter += 1;
-		for(var i = 0; i < tableCells.length; i++) {
 			if (resetCounter % 2 === 0) {
-				tableCells[i].style.backgroundColor="lime";
+				squares[i].style.backgroundColor="lime";
 			}
 			else {
-				tableCells[i].style.backgroundColor="violet";
+				squares[i].style.backgroundColor="violet";
 			}
 		}
+		checkWinner = false;
 	});
 
 
@@ -50,6 +41,10 @@ document.addEventListener("DOMContentLoaded", function(){
 		// console.log(squares[i].innerText);
 		squares[i].addEventListener("click",function(){
 			// alert(this.innerText);
+			if (checkWinner) {
+				alert("There is a Winner, click reset for new game.");
+				return;
+			}
 			if ((this.innerText === "X") || (this.innerText === "O")) {
 				return;
 			}
@@ -60,6 +55,7 @@ document.addEventListener("DOMContentLoaded", function(){
 			else {
 				this.innerText = "O";
 			}
+			this.style.backgroundColor="lightskyblue";
 			winner();
 		});
 	}
